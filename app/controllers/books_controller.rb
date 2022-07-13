528f3b34c346e7247fad7620965d9883a100c424
EC2 Default User <ec2-user@ip-172-31-75-144.ec2.internal>
2022-07-13 17:29:28 +0000

    課題7/14 02:28

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-      if book.save
-        flash[:notice] = "Book was successfully created"
-        redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+      if @book.save
+        redirect_to book_path(@book.id), notice:"Book was successfully created."
       else
-        flash[:alret] = "2 errors prohibited this book from being saved:"
         @books = Book.all
         render "index"
       end
@@ -31,15 +29,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to action: :index
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice:"Book was successfully updated."
+    else
+    render "edit"
+    end
   end
 
   def destroy
-    book = Book.find(params[:id])
+    if book = Book.find(params[:id])
     book.destroy
-    redirect_to action: :index
+    redirect_to "/books", notice:"Book was successfully destroyed."
+    else
+    render :index
+    end
   end
 
   private
